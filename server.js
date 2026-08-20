bbc757a1934ae17e264561c60077687b8aa523fc
enzogunten <enzo.guntendorfer@estudante.ifms.edu.br>
2026-08-20 17:38:45 +0000

    edit: adicionei o consonsole.log que mostra a data do sevidor

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const server = http.createServer();
 
 server.on('request', (req, res) => {
     console.log(`Requisição recebida! ${req.method} ${req.url}`);
-
+    console.log(new Date().toISOString())
     res.statusCode = 201
     res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    res.end("recurso criado");    
+    res.end("recurso criado");
 });
 
-server.listen(porta, ()=> {
+server.listen(porta, () => {
     console.log(`Servidor ouvindo na porta ${porta}`)
 });
 
